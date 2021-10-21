ceb817219ffb40a4734ddbc746ff7e6dbb8551b2
mdeslippe <mdeslippe76@gmail.com>
2021-10-21 14:12:16 -0500

    Page reformatted, and centered hero section. Also fixed the hover on the see projects button, it no longer pushes elements above and below it when the users mouse hovers on it

--- app.js
+++ app.js
@@ -5,49 +5,13 @@ const heroLine = document.querySelector(".heroHeaderLine");
 const contactMeInfo = document.querySelector(".contactMeInfo");
 
 contactMe.addEventListener("mouseover", () => {
-    heroLine.classList.add("noOpa");
-    contactMeInfo.classList.add("yesOpa");
+  heroLine.classList.add("noOpa");
+  contactMeInfo.classList.add("yesOpa");
 });
 
 contactMe.addEventListener("mouseout", () => {
-    setTimeout(() => {
-        heroLine.classList.remove("noOpa");
-        contactMeInfo.classList.remove("yesOpa");
-    }, 5000);
-});
-
-const circleHover = document.querySelector(".bigCircle");
-
-circleHover.addEventListener("mouseover", () => {
-    circleHover.classList.add("bigForHover");
-});
-
-circleHover.addEventListener("mouseout", () => {
-    circleHover.classList.remove("bigForHover");
-});
-
-const copyEmail = document.querySelector(".copyEmail");
-
-copyEmail.addEventListener("mouseover", () => {
-    copyEmail.classList.add("underlineMail");
-});
-
-copyEmail.addEventListener("mouseout", () => {
-    copyEmail.classList.remove("underlineMail");
-});
-
-copyEmail.addEventListener("click", () => {
-    navigator.clipboard.writeText("hassanusaidahmad@gmail.com");
-    document.getElementById("contact-id").innerHTML =
-        "Email copied! Cant wait to hear from you";
-});
-
-const defaultMail = document.querySelector(".openMail");
-
-defaultMail.addEventListener("mouseover", () => {
-    defaultMail.classList.add("underlineMail");
-});
-
-defaultMail.addEventListener("mouseout", () => {
-    defaultMail.classList.remove("underlineMail");
+  setTimeout(() => {
+    heroLine.classList.remove("noOpa");
+    contactMeInfo.classList.remove("yesOpa");
+  }, 5000);
 });
